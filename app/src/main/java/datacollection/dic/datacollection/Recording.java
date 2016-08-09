3033c35b60143715fa88eb7c5cfab2d156e52136
Anubhav <anubhavj1996@gmail.com>
2016-08-10 00:14:46 +0530

    Message.java :Java Class to facilitate display of toasts. Recording.java : Java Class for  recording implementation

---
+++ app/src/main/java/datacollection/dic/datacollection/Recording.java
@@ -0,0 +1,16 @@
+package datacollection.dic.datacollection;
+
+
+import android.media.AudioFormat;
+
+public class Recording {
+
+    private static final int RECORDER_BPP = 16;
+    private static final String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
+    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
+    private static final int[] RECORDER_SAMPLERATE = {8000, 16000, 44100, 48000};
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    short[] audioData;
+}
